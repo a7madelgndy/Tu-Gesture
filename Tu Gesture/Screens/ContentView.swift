47f5de8520652eab9302715cc1f4cc0be2495967
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-24 15:51:15 +0200

    adding the main Pricture and Double Tap Gesture to zoom in

--- Tu Gesture/Screens/ContentView.swift
+++ Tu Gesture/Screens/ContentView.swift
@@ -8,17 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: PROPERTy
+    @State private var isAnimation:Bool = false
+    @State private var scaleImage:CGFloat = 1
+    //MARK: FCNCtion
+    //MARK: CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(20)
+                    .padding()
+                    .opacity(isAnimation ? 1 : 0)
+                    .blur(radius: isAnimation ? 0 : 3)
+                    .animation(.linear(duration: 1), value: isAnimation)
+                    .scaleEffect(scaleImage)
+                    .onTapGesture(count: 2, perform: {
+                        if scaleImage == 1 {
+                            withAnimation(.spring()) {
+                                scaleImage = 2
+                            }
+                        }else {
+                            withAnimation(.spring()) {
+                                scaleImage = 1
+                            }
+                        }//: GESTURE
+                    })
+                
+                 
+            }//: ZSTACK
+            .navigationTitle("Zoom in ")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                isAnimation.toggle()
+            })
+            
+        }//:NAGIGATON
+        .navigationViewStyle(.stack)
+     
     }
 }
 
+
+//MARK: PREVIEW
 #Preview {
     ContentView()
 }
