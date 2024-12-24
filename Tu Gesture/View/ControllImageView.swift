0d715d605b0f91a215ecf5cf6dd26d041234a290
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-24 21:43:22 +0200

    add the  Controller Section and implement the three controllers buttons

---
+++ Tu Gesture/View/ControllImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ControllImageView.swift
+//  Tu Gesture
+//
+//  Created by Ahmed El Gndy on 24/12/2024.
+//
+
+import SwiftUI
+
+struct ControllImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControllImageView()
+}
