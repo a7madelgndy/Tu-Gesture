de33e38c7d30b669f52d4c2d51957dfada21d604
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-24 14:21:03 +0200

    Initial Commit

---
+++ Tu Gesture/Tu_GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_GestureApp.swift
+//  Tu Gesture
+//
+//  Created by Ahmed El Gndy on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tu_GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
